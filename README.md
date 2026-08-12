dea2858345edd1212d6b6fa56f06855badf315b6
Giorgos Sfikas <georges.sfikas@gmail.com>
2026-08-13 02:36:49 +0300

    Produces a nice segmentation result with MobileSAM (40mb)

--- README.md
+++ README.md
@@ -4,3 +4,10 @@
 ### Απαιτούμενα
 
 * python3
+* numpy
+* torch
+* tqdm
+* timm # (for mobileSAM)
+* pip3 install opencv-python
+* pip3 install git+https://github.com/ChaoningZhang/MobileSAM.git
+* pip3 install git+https://github.com/facebookresearch/segment-anything.git #segment-anything==1.0
--- uniwaPointDetector.py
+++ uniwaPointDetector.py
@@ -9,6 +9,11 @@
 
 import logging
 import argparse
+import os
+import urllib.request
+import cv2
+from tqdm import tqdm
+from mobile_sam import sam_model_registry, SamAutomaticMaskGenerator
 
 
 def pointDetector(
@@ -16,8 +21,47 @@ def pointDetector(
         numVerticalPoints,
         numGeneralPoints,
         randomSeed = 1):
-    # Call SAM to get a segmentation of the input.
-    # Alternatively, get DINOv2 to get a feature map, and use it with k-means to get the salient object mask.
+
+    try:
+        image = cv2.cvtColor(cv2.imread(inputImage), cv2.COLOR_BGR2RGB)
+        h, w = image.shape[:2]
+        if w != 1024:
+            new_h = int(round(h * 1024 / w))
+            image = cv2.resize(image, (1024, new_h), interpolation=cv2.INTER_AREA)
+    except Exception as e:
+        print(f"Πρόβλημα κατά την φόρτωση της εικόνας {inputImage}: {e}")
+        return None, None
+
+    checkpoint_path = "mobile_sam.pt"
+    if not os.path.exists(checkpoint_path):
+        url = f"https://github.com/ChaoningZhang/MobileSAM/raw/master/weights/{checkpoint_path}"
+        print(f"Downloading {checkpoint_path}...")
+        with tqdm(unit='B', unit_scale=True, unit_divisor=1024, miniters=1, desc=checkpoint_path) as t:
+            def reporthook(blocknum, blocksize, totalsize):
+                if totalsize is not None:
+                    t.total = totalsize
+                t.update(blocknum * blocksize - t.n)
+            urllib.request.urlretrieve(url, checkpoint_path, reporthook=reporthook)
+
+    sam = sam_model_registry["vit_t"](checkpoint=checkpoint_path)
+    sam.to(device="cpu")
+    sam.eval()
+    mask_generator = SamAutomaticMaskGenerator(
+        sam,
+        points_per_side=8,
+        points_per_batch=32
+    )
+    masks = mask_generator.generate(image)
+
+    if masks:
+        sorted_masks = sorted(masks, key=lambda x: x['area'], reverse=True)
+        foreground_mask = sorted_masks[0]['segmentation']
+        background_mask = ~foreground_mask
+        print(f'saving "binarization.png"')
+        cv2.imwrite('binarization.png', foreground_mask.astype('uint8') * 255)
+    else:
+        foreground_mask, background_mask = None, None
+
 
     return None, None
 
